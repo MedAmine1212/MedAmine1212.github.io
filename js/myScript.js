aa9e70d9e86137310483b4b9d43c660765da91c1
MedAmine1212 <khaili.amine@hotmail.fr>
2022-06-05 19:46:30 +0100

    allDone <3

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,7 @@
 function showEnglish() {
+
+	document.getElementById("downloadBut").href="files/Med Amine Khaili cv-english.pdf";
+	url = "files/Med Amine Khaili cv-english.pdf";
 	document.getElementById("french").style.display="none";
 	document.getElementById("frSpan").style.borderBottom="none";
 	document.getElementById("english").style.display="inherit";
@@ -6,7 +9,7 @@ function showEnglish() {
 }
 
 function showFrench() {
-	
+	document.getElementById("downloadBut").href="files/Med Amine Khaili cv-français.pdf";
 	document.getElementById("english").style.display="none";
 	document.getElementById("engSpan").style.borderBottom="none";
 	document.getElementById("french").style.display="inherit";
